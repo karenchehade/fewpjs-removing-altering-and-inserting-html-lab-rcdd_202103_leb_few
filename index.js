65eeae5fcdff7a400ea840d5b7e8594f41130247
karen chehade <karen_chehade@hotmail.com>
2021-04-11 07:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 main.remove();
 
+// let newHeader = document.createElement('h1#victory')
+// document.body.appendChild(newHeader)
+
 let newHeader = document.createElement('h1#victory')
 document.body.appendChild(newHeader)
