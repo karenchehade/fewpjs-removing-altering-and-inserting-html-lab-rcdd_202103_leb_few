4b1a6c4c3b732ca8db099ecdd374836b3de3babb
karen chehade <karen_chehade@hotmail.com>
2021-04-11 07:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 main.remove();
 
-// let newHeader = document.createElement('h1#victory')
-// document.body.appendChild(newHeader)
-
 let newHeader = document.createElement('h1')
 newHeader.setAttribute("id", "victory")
 newHeader.innerHTML = "Karen is the champion"
